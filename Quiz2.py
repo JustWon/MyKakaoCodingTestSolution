0e6deadcd69806cc3404275582e5db4284863567
DongwonShin <dongwonshin@gist.ac.kr>
2017-09-27 22:17:48 +0900

    My Solution for Quiz2

---
+++ Quiz2.py
@@ -0,0 +1,102 @@
+class Node:
+	def __init__(self, _key, _val, _prev=None, _next=None):
+		self.key = _key
+		self.val = _val
+		self.prev = _prev
+		self.next = _next
+
+class LRUCache:
+
+	# @param capacity, an integer
+	def __init__(self, capacity):
+		self.counter = 0
+		self.capacity = capacity
+		self.dic = dict()
+		self.head = Node(None, None)  # OLD(least recent)
+		self.tail = Node(None, None)  # MOST RECENT
+		self.head.next = self.tail
+		self.tail.prev = self.head
+
+	# @return an integer
+	def get(self, key):
+		ret = -1
+		if key in self.dic:
+			t_node = self.dic[key]
+			ret = t_node.val
+			if t_node != self.tail.prev:
+				# 현재 노드 제거
+				t_prev_node = t_node.prev
+				t_next_node = t_node.next
+				t_prev_node.next = t_next_node
+				t_next_node.prev = t_prev_node
+
+				# 맨 뒤에 붙인다.
+				most_recent_node = self.tail.prev
+				most_recent_node.next = t_node
+				t_node.prev = most_recent_node
+				self.tail.prev = t_node
+				t_node.next = self.tail
+		return ret
+
+	# @param key, an integer
+	# @param value, an integer
+	# @return nothing
+	def set(self, key, value):
+		if key in self.dic:
+			target_node = self.dic[key]
+			target_node.val = value
+			t_prev_node = target_node.prev
+			t_next_node = target_node.next
+			t_prev_node.next = t_next_node
+			t_next_node.prev = t_prev_node
+		else:
+			target_node = Node(key, value)
+			# 자료구조에 넣는다.
+			self.dic[key] = target_node
+			self.counter += 1
+
+		# 맨 뒤에 붙인다.
+		most_recent_node = self.tail.prev
+		most_recent_node.next = target_node
+		target_node.prev = most_recent_node
+		self.tail.prev = target_node
+		target_node.next = self.tail
+
+		if self.counter > self.capacity:
+			most_old_node = self.head.next
+			most_old_node.prev = None
+			self.head.next = most_old_node.next
+			self.head.next.prev = self.head
+			most_old_node.next = None
+			self.dic.pop(most_old_node.key)
+			self.counter -= 1
+# 출처:[LeetCode OJ] LRU Cache - Hard
+
+def Quiz2Solver(cacheSize, cities):
+
+    if (not (0 <= cacheSize and cacheSize <=30)):
+        print("invalid cache size")
+        return
+
+    lru = LRUCache(cacheSize)
+    cities = [c.lower() for c in cities]
+    cache_hit = 1
+    cache_miss = 5
+
+    elapsed_time = 0
+    for i, city in enumerate(cities):
+        if (i < cacheSize): # initial caching
+            lru.set(city, city)
+            elapsed_time += cache_miss
+            # print("%s cache init" % city)
+        else:
+            if(lru.get(city) != -1): # cache hit
+                lru.set(city,city)
+                elapsed_time += cache_hit
+                # print("%s cache hit" % city)
+            else:    # cache miss
+                lru.set(city, city)
+                elapsed_time += cache_miss
+                # print("%s cache miss" % city)
+
+    return elapsed_time
---
+++ Test2.py
@@ -0,0 +1,38 @@
+import unittest
+import Quiz2
+
+class KakaoQuiz1Test(unittest.TestCase):
+
+    def test1(self):
+        cacheSize = 3
+        cities = ['Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA', 'Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA']
+        print(Quiz2.Quiz2Solver(cacheSize, cities))
+
+    def test2(self):
+        cacheSize = 3
+        cities = ['Jeju', 'Pangyo', 'Seoul', 'Jeju', 'Pangyo', 'Seoul', 'Jeju', 'Pangyo', 'Seoul']
+        print(Quiz2.Quiz2Solver(cacheSize, cities))
+
+    def test3(self):
+        cacheSize = 2
+        cities = ['Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA', 'SanFrancisco', 'Seoul', 'Rome', 'Paris', 'Jeju', 'NewYork', 'Rome']
+        print(Quiz2.Quiz2Solver(cacheSize, cities))
+
+    def test4(self):
+        cacheSize = 5
+        cities = ['Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA', 'SanFrancisco', 'Seoul', 'Rome', 'Paris', 'Jeju', 'NewYork', 'Rome']
+        print(Quiz2.Quiz2Solver(cacheSize, cities))
+
+    def test5(self):
+        cacheSize = 2
+        cities = ['Jeju', 'Pangyo', 'NewYork', 'newyork']
+        print(Quiz2.Quiz2Solver(cacheSize, cities))
+
+    def test6(self):
+        cacheSize = 0
+        cities = ['Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA']
+        print(Quiz2.Quiz2Solver(cacheSize, cities))
+
+if __name__=='__main__':
+    unittest.main()
+
